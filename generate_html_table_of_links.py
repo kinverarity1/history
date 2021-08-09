dd0e2100601e3463a7b7bd3eab475c1a94abeb7f
Kent Inverarity <kinverarity@hotmail.com>
2021-08-09 13:59:03 +0930

    update script

--- generate_html_table_of_links.py
+++ generate_html_table_of_links.py
@@ -1,1 +1,5 @@
-line = '<li><a href="https://collections.slsa.sa.gov.au/collection/Acre+1+Collection">Acre 1 Collection</a></li>'
+
+with open("output.txt", "w") as f:
+    for n in range(1, 1043):
+        line = f'<li><a href="https://collections.slsa.sa.gov.au/collection/Acre+{n}+Collection">Acre {n} Collection</a></li>'
+        f.write(f"        {line}\n")
